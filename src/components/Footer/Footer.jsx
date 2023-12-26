cd1cadec5ddcb79882c289f3b88c6d786d845250
Munawar Ali <mnrkokhar@gmail.com>
2023-12-26 22:26:45 +0500

    Refactor Footer component and Newsletter component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,34 +9,34 @@ const Footer = () => {
     <footer className="bg-black/80 text-white py-8">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Contact Details */}
-        <div className="flex flex-col justify-center text-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className='text-center lg:text-left'>
           <h2 className="text-green-700 text-xl font-semibold mb-4">Contact Details</h2>
-          <div className='lg:text-left'>
-            <p>Name: Capital Group</p>
-            <p>Email: capitalgroup@example.com</p>
-            <p>Contact: +123 456 7890</p>
-            <p>Address: 123 Main St, New York</p>
+            <p>Capital Group</p>
+            <p>capitalgroup@example.com</p>
+            <p>+123 456 7890</p>
+            <p>123 Main St, New York</p>
           </div>
         </div>
 
         {/*Desktop Logo */}
-        <div className="hidden lg:block text-center">
+        <div className="hidden lg:block justify-center items-center">
             <Link to='/' key={'home'}>
                 <img src={logo} alt="Logo" className="w-36 md:w-48 md:h-30 mx-auto rounded-lg" />
             </Link>
         </div>
 
         {/* Social Media Links */}
-        <div className="text-center">
+        <div className="flex flex-col justify-center items-center">
+          <div>
           <h2 className="text-green-700 text-xl font-semibold mb-4">Social Media</h2>
-          <div className="flex justify-center space-x-4">
-            <a href="#" className="text-gray-400 hover:text-white">
+            <a href="#" className="mx-2 text-gray-400 hover:text-white">
               <i className="fab fa-facebook fa-lg lg:fa-2x"></i>
             </a>
-            <a href="#" className="text-gray-400 hover:text-white">
+            <a href="#" className="mx-2 text-gray-400 hover:text-white">
               <i className="fab fa-twitter fa-lg lg:fa-2x"></i>
             </a>
-            <a href="#" className="text-gray-400 hover:text-white">
+            <a href="#" className="mx-2 text-gray-400 hover:text-white">
               <i className="fab fa-instagram fa-lg lg:fa-2x"></i>
             </a>
           </div>
@@ -44,9 +44,9 @@ const Footer = () => {
 
         {/*Mobile Logo */}
         <div className="lg:hidden text-center">
-              <Link to='/' key={'home'}>
-                  <img src={logo} alt="Logo" className="w-36 md:w-48 md:h-30 mx-auto rounded-lg" />
-              </Link>
+            <Link to='/' key={'home'}>
+                <img src={logo} alt="Logo" className="w-36 md:w-48 md:h-30 mx-auto rounded-lg" />
+            </Link>
         </div>
 
       </div>
--- src/components/Footer/NewsLetter.jsx
+++ src/components/Footer/NewsLetter.jsx
@@ -4,15 +4,14 @@ import logo from '../../assets/ngc_logo.jpg'
 
 const NewsLetter = () => {
   return (
-    <footer className="bg-black/80 rounded-t-2xl text-white shadow-sm shadow-white">
-      {/* Full-width container */}
-      <div className="w-full">
+    
+      <div className="w-full py-5 bg-gray-700/40 text-white shadow-sm shadow-white">
             {/* Content in the top card */}
-            <div className=" bg-gray-700/40 flex flex-col items-center justify-center h-44 relative z-10">
+            <div className="flex flex-col items-center justify-center h-44 relative z-10">
               {/* Form */}
               <form className="flex flex-col items-center">
                 <label className="text-xl">Subscribe to Newsletter</label>
-                <p className="text-center mt-4 text-slate-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="text-center mt-4 text-slate-400">Subscribe to this news letter to get daily update</p>
                 <div className='flex flex-col md:flex-row m-1 items-center'>
                 <input type="email" placeholder="Your email" className="w-full px-4 py-2 mx-2 my-2 rounded bg-black/60 text-white" />
                 <button type="submit" className="bg-light-green-800 text-white px-4 py-2 rounded  hover:bg-green-900">Subscribe</button>
@@ -20,7 +19,6 @@ const NewsLetter = () => {
               </form>
             </div>
       </div>
-    </footer>
   );
 };
 
