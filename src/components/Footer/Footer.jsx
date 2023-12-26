e974ca6ce8c7158a9fa4cf2f04edf3cb3ffe8337
Munawar Ali <mnrkokhar@gmail.com>
2023-12-26 22:01:50 +0500

    Update Footer component layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
     <footer className="bg-black/80 text-white py-8">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Contact Details */}
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center text-center">
           <h2 className="text-green-700 text-xl font-semibold mb-4">Contact Details</h2>
-          <div className='text-left'>
+          <div className='lg:text-left'>
             <p>Name: Capital Group</p>
             <p>Email: capitalgroup@example.com</p>
             <p>Contact: +123 456 7890</p>
@@ -19,8 +19,8 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Logo */}
-        <div className="text-center">
+        {/*Desktop Logo */}
+        <div className="hidden lg:block text-center">
             <Link to='/' key={'home'}>
                 <img src={logo} alt="Logo" className="w-36 md:w-48 md:h-30 mx-auto rounded-lg" />
             </Link>
@@ -31,16 +31,24 @@ const Footer = () => {
           <h2 className="text-green-700 text-xl font-semibold mb-4">Social Media</h2>
           <div className="flex justify-center space-x-4">
             <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-facebook fa-2x"></i>
+              <i className="fab fa-facebook fa-lg lg:fa-2x"></i>
             </a>
             <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-twitter fa-2x"></i>
+              <i className="fab fa-twitter fa-lg lg:fa-2x"></i>
             </a>
             <a href="#" className="text-gray-400 hover:text-white">
-              <i className="fab fa-instagram fa-2x"></i>
+              <i className="fab fa-instagram fa-lg lg:fa-2x"></i>
             </a>
           </div>
         </div>
+
+        {/*Mobile Logo */}
+        <div className="lg:hidden text-center">
+              <Link to='/' key={'home'}>
+                  <img src={logo} alt="Logo" className="w-36 md:w-48 md:h-30 mx-auto rounded-lg" />
+              </Link>
+        </div>
+
       </div>
 
       {/* All Rights Reserved */}
